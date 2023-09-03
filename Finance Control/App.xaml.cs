e4ce130befd65dfca7228ecfb2d0b200bb93b2a5
Hitallo Gondim <italo200062@gmail.com>
2023-09-03 15:42:46 -0300

    commit initial

---
+++ Finance Control/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Finance_Control
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Finance Control/View/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Finance_Control
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        
+    }
+}
